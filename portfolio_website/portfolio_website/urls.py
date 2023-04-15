ca5e92fc133406d203e4be58170586af78703676
le0pard <loganfinnimore@gmail.com>
2023-04-15 15:51:20 +1200

    import error/

--- portfolio_website/portfolio_website/urls.py
+++ portfolio_website/portfolio_website/urls.py
@@ -15,13 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 
-from posts.views import home_view
+from posts.views import home_view, post_detail_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
     path('', home_view),
-    path('posts/<int:post_id>', home_view)
+    path('posts/<int:post_id>', post_detail_view)
 ]
--- portfolio_website/posts/views.py
+++ portfolio_website/posts/views.py
@@ -1,6 +1,15 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from .models import Post
 # Create your views here.
 def home_view(request, *args, **kwargs):
+    print(args, kwargs)
     return HttpResponse("<h1>Hello</h1>")
+
+def post_detail_view(request, post_id, *args, **kwargs):
+    try:
+        obj = Post.objects.get(id=post_id)
+    except:
+        print("error")
+    return HttpResponse(f"<h1>Hello {post_id} - {obj.content} </h1>")
