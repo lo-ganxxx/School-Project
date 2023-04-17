46f27dd07e65bf697203ea7ff4bce7763bc86704
le0pard <loganfinnimore@gmail.com>
2023-04-17 13:44:50 +1200

    put template into use for homepage, with navbar

--- portfolio_website/posts/views.py
+++ portfolio_website/posts/views.py
@@ -1,15 +1,25 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404, JsonResponse
 from django.shortcuts import render
 
 from .models import Post
 # Create your views here.
 def home_view(request, *args, **kwargs):
-    print(args, kwargs)
-    return HttpResponse("<h1>Hello</h1>")
+    #print(args, kwargs)
+    #return HttpResponse("<h1>Hello</h1>")
+    return render(request, "pages/home.html", context={}, status=200)
 
 def post_detail_view(request, post_id, *args, **kwargs):
+    data = {
+        "id": post_id,
+        #"image_path": obj.image.url
+    }
+    status = 200
     try:
         obj = Post.objects.get(id=post_id)
+        data['content'] = obj.content
     except:
-        return HttpResponse("<h1>Error</h1>")
-    return HttpResponse(f"<h1>Hello {post_id} - {obj.content}</h1>")
+        data['message'] = "Not found"
+        status = 404
+    
+    return JsonResponse(data, status=status)
+    #return HttpResponse(f"<h1>Hello {post_id} - {obj.content}</h1>")
