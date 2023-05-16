ebcbea6a428d5fd934a61d2f850ac83b0b8ddc91
le0pard <79114731+le0parddd@users.noreply.github.com>
2023-05-16 12:14:04 +1200

    Handling ajax requests for creating new posts and serializing post model objects

--- portfolio_website/posts/views.py
+++ portfolio_website/posts/views.py
@@ -16,11 +16,14 @@ def home_view(request, *args, **kwargs):
     return render(request, "pages/home.html", context={}, status=200)
 
 def create_post(request, *args, **kwargs):
+    print("ajax", is_ajax(request=request))
     form = PostForm(request.POST or None) #this means that if the request is not a POST but instead a GET (e.g. loading up the page initially), it is a fallback value so that it doesnt just submit no data
     next_url = request.POST.get("next") or None #getting the next url from the POST request or just None if it was not declared
     if form.is_valid(): #if form doesnt return a ValidationError
         obj = form.save(commit=False) #creates a new Post object with the form data by calling the save() method on the form, passing in commit=False to prevent it from immediately being saved to the database
         obj.save() #saves the Post object to database
+        if is_ajax(request=request):
+            return JsonResponse(obj.serialize(), status=201) # 201 == created post
         if next_url != None and url_has_allowed_host_and_scheme(next_url, ALLOWED_HOSTS): #If there is a next url declared
             return redirect(next_url) #Redirect user to that url
         form = PostForm #clears the form for next submitting
@@ -28,7 +31,7 @@ def create_post(request, *args, **kwargs):
 
 def post_list_view(request, *args, **kwargs):
     qs = Post.objects.all()
-    posts_list = [{"id": x.id, "content": x.content, "likes": random.randint(0,1000)} for x in qs]
+    posts_list = [x.serialize() for x in qs]
     data = {
         "isUser": False,
         "response": posts_list
@@ -50,3 +53,6 @@ def post_detail_view(request, post_id, *args, **kwargs):
     
     return JsonResponse(data, status=status)
     #return HttpResponse(f"<h1>Hello {post_id} - {obj.content}</h1>")
+
+def is_ajax(request): #Had to create custom function to check as is_ajax() method was deprecated
+    return request.META.get('HTTP_X_REQUESTED_WITH') == 'XMLHttpRequest' #Return boolean
