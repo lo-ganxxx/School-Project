bfd172bda2fb9acb37851aaf0ec8671fc44d2cdf
le0pard <79114731+le0parddd@users.noreply.github.com>
2023-05-23 12:12:15 +1200

    Adding a like field to post model (ManyToManyField)

--- portfolio_website/posts/admin.py
+++ portfolio_website/posts/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Post
+from .models import Post, PostLike
+
+class PostLikeAdmin(admin.TabularInline):
+    model = PostLike
 
 class PostAdmin(admin.ModelAdmin):
+    inlines = [PostLikeAdmin]
     list_display = ['__str__', 'user', 'content']
     search_fields = ['content', 'user__username', 'user__email']
     class Meta:
--- portfolio_website/posts/models.py
+++ portfolio_website/posts/models.py
@@ -4,12 +4,19 @@ import random
 
 User = settings.AUTH_USER_MODEL
 
+class PostLike(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey("Post", on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
 # Create your models here.
 class Post(models.Model):
     #id = models.AutoField(primary_key=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE) #one user can have many posts, if user is deleted, all their posts are deleted too
+    likes = models.ManyToManyField(User, related_name='post_user', blank=True, through=PostLike) #one post can have many users and many users can have many posts (in relation to likes)
     content = models.TextField(blank=True, null=True)
     image = models.FileField(upload_to="images/", blank=True, null=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return "Post ID: " + str(self.id)
