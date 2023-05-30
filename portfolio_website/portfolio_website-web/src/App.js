740799fa0f0c3727a9feedc1bf04c8f8ed79b0c2
le0pard <79114731+le0parddd@users.noreply.github.com>
2023-05-30 13:07:20 +1200

    starting learning functional components

--- portfolio_website/portfolio_website-web/src/App.js
+++ portfolio_website/portfolio_website-web/src/App.js
@@ -1,13 +1,25 @@
+import {useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [post, setPosts] = useState([])
+  useEffect(() => {
+    // do my lookup
+    const postItems = [{"content": 123}, {"content": "hello world"}]
+    setPosts(postItems)
+  }, [])
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <p>
+          {postMessage.map((post, index)=>{
+            return <li>{post.content}</li>
+          })}
         </p>
         <a
           className="App-link"
