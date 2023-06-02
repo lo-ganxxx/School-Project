10bee5a4a03bf6789a0c7a367a63be37cb559232
le0pard <loganfinnimore@gmail.com>
2023-06-02 22:00:16 +1200

    Like button with React

--- portfolio_website/portfolio_website-web/src/App.js
+++ portfolio_website/portfolio_website-web/src/App.js
@@ -20,11 +20,22 @@ function loadPosts(callback) {
   xhr.send()
 }
 
+function ActionBtn(props) {
+  const {post, action} = props //takes the post
+  const className = props.className ? props.className : 'btn btn-primary btn-small'
+  return action === 'like' ? <button className={className}>{post.likes} Likes</button> : null
+}
+
 function Post(props) {
-  const {post} = props
-  const className = props.className ? props.className : 'col-10 max-auto col-md-6'
+  const {post} = props // This line extracts the post prop from the props object using destructuring assignment. It allows the component to access the post prop directly without having to reference props.post throughout the component.
+  const className = props.className ? props.className : 'col-10 max-auto col-md-6' // if the props object has a className prop it will use that, otherwise will use default value
   return <div className={className}>
     <p>{post.content} - {post.id}</p>
+    <div className = 'btn btn-group'>
+      <ActionBtn post={post} action={{type: "like"}}/>
+      <ActionBtn post={post} action={{type: "unlike"}}/>
+      <ActionBtn post={post} action={{type: "comment"}}/>
+    </div>
   </div>
 }
 
