d33fec4c09b8796f097936225671bea0227df58b
le0pard <loganfinnimore@gmail.com>
2023-06-13 12:09:13 +1200

    Like rendering error fixed - missing $, was mixing up different posts because they will have the same key!

--- portfolio_website/portfolio_website-web/src/posts/components.js
+++ portfolio_website/portfolio_website-web/src/posts/components.js
@@ -37,49 +37,6 @@ export function PostsComponent(props) {
   </div>
 }
 
-export function PostsComponentNew(props) { //FOR TRYING TO FIX THE LIKES ISSUE
-  const textAreaRef = createRef() //reference for the text area (used to access the value of the text area input)
-  const [newPosts, setNewPosts] = useState([])
-  const handleBackendUpdate = (response, status) => { //backend api response handler
-    if (status === 201) {
-      // Update the likes count for the new post in the response
-      const newPost = { ...response, likes: 0}
-      const updatedNewPosts = [newPost, ...newPosts]
-      setNewPosts(updatedNewPosts)
-
-      // Update the likes count for the existing posts in the postsInit state
-      const updatedPostsInit = postsInit.map((post) => {
-        if (post.id === response.id) {
-          return newPost
-        }
-        return post
-      })
-      setPostsInit(updatedPostsInit)
-    } else {
-      console.log(response)
-      alert("An error occured, please try again")
-    }
-  }
-  const handleSubmit = (event) => {
-    event.preventDefault()
-    const newVal = textAreaRef.current.value //the text being submitted (to post)
-    //backend api request
-    apiPostCreate(newVal, handleBackendUpdate) //handleBackendUpdate is a callback function - once the operation of the createPost function is complete and the response is received from the backend, the createPost/lookup function will invoke the callback function handleBackendUpdate and passes the response and status as arguments
-    textAreaRef.current.value = '' //clear the text box
-  }
-  return <div className={props.className}>
-    <div className='col-md-4 mx-auto col-10'>
-      <form onSubmit={handleSubmit}>
-        <textarea ref={textAreaRef} required={true} className='form-control' name='post'>
-
-        </textarea>
-        <button type='submit' className='btn btn-primary my-3'>Post</button>
-    </form>
-    </div>
-  <PostsList newPosts={newPosts}/>
-  </div>
-}
-
 export function PostsList(props) {
     const [postsInit, setPostsInit] = useState([]) //inital posts
     const [posts, setPosts] = useState([]) //all posts
@@ -105,7 +62,7 @@ export function PostsList(props) {
     }
     }, [setPostsInit, postsDidSet, setPostsDidSet])
     return posts.map((item, index)=>{ //iterates through list of posts
-      return <Post post={item} className='my-5 py-5 border bg-white text-dark' key={`${index}-{item.id}`} /> //rendering post
+      return <Post post={item} className='my-5 py-5 border bg-white text-dark' key={`${index}-${item.id}`} /> //rendering post
     })
   }
 
