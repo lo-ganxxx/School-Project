b8e0d9eb884c1a789bd60ee1d79db967cd45a5ab
le0pard <79114731+le0parddd@users.noreply.github.com>
2023-06-16 11:48:57 +1200

    Post detail page complete in javascript with view buttons on posts

--- portfolio_website/portfolio_website-web/src/posts/detail.js
+++ portfolio_website/portfolio_website-web/src/posts/detail.js
@@ -6,7 +6,15 @@ export function Post(props) {
     const {post} = props // This line extracts the post prop from the props object using destructuring assignment. It allows the component to access the post prop directly without having to reference props.post throughout the component.
     const [actionPost, setActionPost] = useState(props.post ? props.post : null)
     const className = props.className ? props.className : 'col-10 max-auto col-md-6' // if the props object has a className prop it will use that, otherwise will use default value
-    
+    var path = window.location.pathname //getting pages path
+    var idRegex = /(?<postid>\d+)/ //id regular expression
+    var match = path.match(idRegex)
+    const urlPostId = match ? match.groups.postid : -1
+    const isDetail = `${post.id}` === `${urlPostId}`
+    const handleLink = (event) => {
+      event.preventDefault()
+      window.location.href = `/${post.id}` //redirects user to post detail page of the post they clicked on (using the posts id)
+    }
     const handlePerformAction = (newActionPost) => {
       setActionPost(newActionPost) //updates component
     }
@@ -17,6 +25,7 @@ export function Post(props) {
         <ActionBtn post={actionPost} didPerformAction={handlePerformAction} action={{type: "like", display: "Likes"}}/>
         <ActionBtn post={actionPost} didPerformAction={handlePerformAction} action={{type: "unlike", display: "Unlike"}}/>
         <ActionBtn post={actionPost} didPerformAction={handlePerformAction} action={{type: "comment", display: "Comment"}}/>
+        {isDetail === true ? null : <button className='btn btn-outline-primary btn-sm' onClick={handleLink}>View</button>} {/* if isDetail is true it will render nothing (null) otherwise it will render the view button */}
       </div>
     }
     </div>
