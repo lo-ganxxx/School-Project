a8de16b42e87e837fc7a5c701dd6671a35e43c17
le0pard <79114731+le0parddd@users.noreply.github.com>
2023-06-22 09:47:31 +1200

    Profile update form and view

--- portfolio_website/profiles/urls.py
+++ portfolio_website/profiles/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import profile_detail_view
+from .views import profile_detail_view, profile_update_view
 
 urlpatterns = [
+    path('edit', profile_update_view), #blacklisted username 'edit' to avoid errors
     path('<str:username>', profile_detail_view), #sets username argument to whatever string is in that place in the url
     
 ]
