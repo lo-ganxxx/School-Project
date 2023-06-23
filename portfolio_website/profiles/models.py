2dbc8b18eb0d1fc0847972d618754919ea5e9824
lo-ganxxx <loganfinnimore@gmail.com>
2023-06-23 13:12:29 +1200

    FollowerRelation model

--- portfolio_website/profiles/models.py
+++ portfolio_website/profiles/models.py
@@ -6,10 +6,19 @@ from django.db.models.signals import post_save
 
 User = settings.AUTH_USER_MODEL
 
+class FollowerRelation(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    profile = models.ForeignKey("Profile", on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE) #one user associated with one profile, one profile associated with one user
     nickname = models.CharField(max_length=100, null=True, blank=True)
     bio = models.TextField(blank=True, null=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True) #most recent time it was saved
+    followers = models.ManyToManyField(User, related_name="following", blank=True) #related name is "following" as from the followers perspective this profile is one they are folllowing i.e. user.following.all() -> all users a user follows
+    
 
 def user_did_save(sender, instance, created, *args, **kwargs):
     if created:
