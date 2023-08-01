b946eed198461d4fcf051c6978414c1931dc2d85
lo-ganxxx <loganfinnimore@gmail.com>
2023-08-02 10:53:18 +1200

    Profile search api view

--- portfolio_website/profiles/api/urls.py
+++ portfolio_website/profiles/api/urls.py
@@ -2,10 +2,12 @@ from django.urls import path
 
 from .views import (
     profile_detail_api_view,
+    profile_search_api_view
 )
 
 urlpatterns = [
     path('<str:username>/follow', profile_detail_api_view),
     path('<str:username>/', profile_detail_api_view),
+    path('search/<str:search_query>', profile_search_api_view)
     
 ]
