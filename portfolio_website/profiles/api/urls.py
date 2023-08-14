d4c64f00bd2fafa5a327142a81a748580128b91a
lo-ganxxx <loganfinnimore@gmail.com>
2023-08-14 15:13:58 +1200

    c

--- portfolio_website/profiles/api/urls.py
+++ portfolio_website/profiles/api/urls.py
@@ -8,7 +8,7 @@ from .views import (
 )
 
 urlpatterns = [
-    path('suggested/', profile_suggestions_api_view),
+    path('suggested/', profile_suggestions_api_view), #blacklisted username to avoid errors on api lookup
     path('popular/', profile_popular_api_view),
     path('<str:username>/follow', profile_detail_api_view),
     path('<str:username>/', profile_detail_api_view),
--- portfolio_website/profiles/urls.py
+++ portfolio_website/profiles/urls.py
@@ -4,7 +4,7 @@ from .views import profile_detail_view, profile_update_view, profile_search_view
 
 urlpatterns = [
     path('edit', profile_update_view), #blacklisted username 'edit' to avoid errors
-    path('popular/', profile_popular_view),
+    path('popular/', profile_popular_view), #blacklisted
     path('<str:username>', profile_detail_view), #sets username argument to whatever string is in that place in the url
     path('search/<str:query>', profile_search_view)
 ]
