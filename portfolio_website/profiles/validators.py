f8ee77cb8875249e43ade7db6fdddf3379519478
lo-ganxxx <loganfinnimore@gmail.com>
2023-08-16 12:29:51 +1200

    database change

--- portfolio_website/profiles/validators.py
+++ portfolio_website/profiles/validators.py
@@ -10,5 +10,5 @@ def validate_file_size(value):
     
 def validate_instagram_username(value):
     allowed_characters = set("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_.")
-    if not all(char in allowed_characters for char in value):
+    if not all(char in allowed_characters for char in value): #if not all of the characters in the value are characters in the allowed_characters string
         raise ValidationError("Invalid characters in Instagram username")
