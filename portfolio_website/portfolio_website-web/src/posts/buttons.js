07f14cb49986bad2242448b6c3d086d45b3631c3
lo-ganxxx <loganfinnimore@gmail.com>
2023-08-24 09:49:08 +1200

    Got rid of redundant button component code

--- portfolio_website/portfolio_website-web/src/posts/buttons.js
+++ portfolio_website/portfolio_website-web/src/posts/buttons.js
@@ -1,4 +1,3 @@
-import {createRef} from 'react'
 import {apiPostAction} from './lookup'
 
 export function ActionBtn(props) {
@@ -6,8 +5,6 @@ export function ActionBtn(props) {
     const likes = post.likes ? post.likes : 0
     const className = props.className ? props.className : 'btn btn-primary btn-small'
     const actionDisplay = action.display ? action.display : 'Action' // the display of the button
-    const comment_upload = false //temp for testing
-    const textAreaRef = createRef() //reference for the text area (used to access the value of the text area input)
 
     const handleActionBackendEvent = (response, status) => {
       if ((status === 200 || status === 201) && didPerformAction) { //status 200 OR status 201
@@ -20,11 +17,6 @@ export function ActionBtn(props) {
       if (action.type === 'comment') {
         //open the comment input box and comment upload button
         didCommentForm()
-      } else if (comment_upload === true) {
-        //upload the comment
-        const newVal = textAreaRef.current.value //the text being submitted (to comment)
-        apiPostAction(post.id, 'comment', handleActionBackendEvent, newVal) //apiPostAction with the text areas value being the content of the comment
-        textAreaRef.current.value = '' //clear the text box
       } else {
         apiPostAction(post.id, action.type, handleActionBackendEvent)
       }
